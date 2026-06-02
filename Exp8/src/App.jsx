1e9bdfd6dae2b8b077ed74297a27644dc9aed64b
atharvmishracode <atharv666mishra@gmail.com>
2026-06-03 00:04:28 +0530

    Add Exp8

---
+++ Exp8/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './count.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Counter Application</h1>
+      <h1>{count}</h1>
+      <div className="buon-container">
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
